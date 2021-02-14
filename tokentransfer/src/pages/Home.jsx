17fe1eb87bc9ce206f1af70522d970f60d6cf227
Kurt <Kurt@Merbeth.io>
2021-02-14 17:57:26 +0100

    token transfer

---
+++ tokentransfer/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import { Grid } from "@material-ui/core";
+
+class Home extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <Grid container className="home">
+          Home
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Home;
