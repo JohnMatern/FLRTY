8eea2e6d5dabfaac961afbfeab50a8fa47f9bcd7
Lilith <valerie.vaske@gmail.com>
2021-03-25 13:17:03 +0100

    Rough

--- prototype/frontend/src/App/App.js
+++ prototype/frontend/src/App/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import '../media/Styles/Style.scss'
 
 class App extends Component {
 
   render() {
 
     return (
-      <div classname="app">
-        Hello World, my old friend
+      <div className="app">
+        <div className="login">
+          <h2>Login</h2>
+          <button>Connect with wallet here</button>
+        </div>
+        <br /> 
+        <br /> 
+        <div className="projects">
+          <h2>Projects</h2>
+          <p>List of Projects</p>
+          <br /> 
+          <p>Create Own Project</p>
+          <form>
+            <label>Name</label>
+            <input type='text' /> 
+          </form>
+        </div>
       </div>
     )
   }
