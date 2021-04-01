8842d20891ce66fc4e4837be6da5bba91c21c4bd
Kurt <Kurt@Merbeth.io>
2021-04-01 12:31:35 +0200

    added button

--- prototype/frontend/src/component/Project.jsx
+++ prototype/frontend/src/component/Project.jsx
@@ -1,5 +1,5 @@
 import { React, Component } from 'react';
-const dateOptions = {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+const dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit' };
 
 class Project extends Component {
     constructor(props) {
@@ -21,13 +21,14 @@ class Project extends Component {
     componentDidMount = async () => {
         let project = await this.props.address;
         let projectName = await this.props.project.methods.getName(project).call();
-        let projectDesc  = await this.props.project.methods.getDesc(project).call();
-        let projectCreator  = await this.props.project.methods.getCreator(project).call();
+        let projectDesc = await this.props.project.methods.getDesc(project).call();
+        let projectCreator = await this.props.project.methods.getCreator(project).call();
         let group = await this.props.project.methods.getGroup(project).call();
         let groupName = await this.props.group.methods.getName(group).call();
         let votes = await this.props.vote.methods.balanceOf(project).call();
         let minVotes = await this.props.project.methods.getMinVotes(project).call();
         let endDate = await this.props.project.methods.getEndDate(project).call();
+        let userVotes = await this.props.store.methods.getVoteHistory(project, this.props.account);
 
         this.setState({
             project,
@@ -42,21 +43,34 @@ class Project extends Component {
         })
     }
 
+    handleChange = () => {
+
+    }
+
+    handleSubmit = () => {
+
+    }
+
     render() {
-            return (
-                <div className="projects">
-                    <div>
-                        Name: {this.state.projectName} <br />
+        return (
+            <div className="projects">
+                <div>
+                    Name: {this.state.projectName} <br />
                         Address:  {this.state.project} <br />
                         Description:  {this.state.projectDesc} <br />
                         Creator:  {this.state.projectCreator} <br />
                         Group:  {this.state.groupName} <br />
                         Min Votes:  {this.state.minVotes} <br />
                         Actual Votes:  {this.state.votes} <br />
-                        End Date:  {new Date(this.state.endDate*1000).toLocaleDateString('de-DE', dateOptions)} <br />
-                    </div>
+                        End Date:  {new Date(this.state.endDate * 1000).toLocaleDateString('de-DE', dateOptions)} <br />
+                    Vote: <input
+                        name="vote"
+                        type="text"
+                        onChange={this.handleChange} />
+                    <button onClick={this.handleSubmit} disabled={!this.state.button}>coming soon...</button> <br />
                 </div>
-            )
+            </div>
+        )
     }
 }
 
